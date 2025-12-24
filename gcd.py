79bbc18d3678fab358d9d5a53f610f898fd8040c
shrujankulkarni <shrujan19@gmail.com>
2025-12-24 22:38:14 +0530

    commit

---
+++ gcd.py
@@ -0,0 +1,19 @@
+# Given an array of n positive integers,
+# find the GCD of all the array elements.
+
+'''
+Docstring for gcd
+Example :
+
+Input: n = 3, arr = [1, 2, 3]
+Output: 1
+Explanation: GCD of 1,2,3 is 1.
+'''
+
+def gcd(n):
+    for i in range(1,n):
+        if(n%i==0):
+            gcd=i
+        return gcd
+    
+def find_gcd(arr, n):

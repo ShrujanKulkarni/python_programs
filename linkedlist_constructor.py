6f595fefbf6fd42079d41f36fb7902b9be73a3d1
shrujankulkarni <shrujan19@gmail.com>
2026-01-04 22:55:28 +0530

    commit

--- linkedlist_constructor.py
+++ linkedlist_constructor.py
@@ -10,6 +10,11 @@ class LinkedList:
         self.tail=newnode
         self.length=1
 
-mylinkedlist = LinkedList(9)
+mylinkedlist = LinkedList(10)
 print(mylinkedlist.head.value)
-        
+
+def printlist(self):
+    temp=self.head
+    while temp is not None:
+        print(temp.value)
+        temp=temp.next

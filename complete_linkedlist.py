f17036210cd9f948aec89d3722ba1847573244c8
shrujankulkarni <shrujan19@gmail.com>
2026-01-08 20:58:36 +0530

    node, print

--- complete_linkedlist.py
+++ complete_linkedlist.py
@@ -2,5 +2,21 @@ class Node:
     def __init__(self,value):
         self.value= value
         self.next= None
-        self.length=1
 
+class Linkedlist:
+    def __init__(self,value):
+        self.length=0
+        newnode=Node(value)
+        self.head=newnode
+        self.tail=newnode
+        self.length+=1
+        # return newnode
+
+def printlist(self):
+    temp= self.head
+    while(temp):
+        print(temp.value)
+        temp=temp.next
+
+mylinkedlist = Linkedlist(10)
+print(mylinkedlist.head.value)

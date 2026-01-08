0da812f5835256d001ebd2f2fe438440f5908126
shrujankulkarni <shrujan19@gmail.com>
2026-01-08 22:51:04 +0530

    pop

--- complete_linkedlist.py
+++ complete_linkedlist.py
@@ -25,14 +25,35 @@ class Linkedlist:
     def printlist(self):
         temp= self.head
         while(temp):
-            print(temp.value)
+            print(temp.value,"-->", end="  ")
             temp=temp.next
+
+    def pop(self):
+        if self.length==0:
+            return None
+        else:
+            temp=pre=self.head
+            while(temp.next is not None):
+                pre=temp
+                temp=temp.next
+            self.tail=pre
+            self.tail.next=None 
+            self.length-=1
+
+            if self.length==0:
+                self.head=self.tail=None
+        return temp.value
 
 mylinkedlist = Linkedlist(10)
 mylinkedlist.append(20)
 mylinkedlist.append(30)
+# mylinkedlist.append(40)
 mylinkedlist.append(40)
-# mylinkedlist.printlist()
-mylinkedlist.append(50)
 mylinkedlist.printlist()
+
+print("----------After Pop()----------")
+popvalue= mylinkedlist.pop()
+mylinkedlist.printlist()
+print(f"\nPopped values is:{popvalue}")
+
 

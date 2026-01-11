066f2c1436aca91e0c2aa2bb6283696ff4f24d62
shrujankulkarni <shrujan19@gmail.com>
2026-01-11 22:05:15 +0530

    commit

---
+++ dll_constructor.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+        self.prev=None
+
+class DoublyLinkedList:
+    def __init__(self,value):
+        newnode=Node(value)
+        self.head=newnode
+        self.tail=newnode
+        self.length=1
+
+    def printlist(self):
+        curr=self.head
+        while curr is not None:
+            print(curr.value)
+            curr=curr.next
+
+
+dll=DoublyLinkedList(9)
+dll.printlist()
+

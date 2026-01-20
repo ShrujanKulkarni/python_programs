14c57b7286498931cb909975b7b8fbd25e232374
shrujankulkarni <shrujan19@gmail.com>
2026-01-20 22:26:20 +0530

    .

--- REVISION/ll.py
+++ REVISION/ll.py
@@ -2,3 +2,11 @@ class Node:
     def __init__(self,value):
         self.value=value
         self.next=None
+
+
+class LinkedList:
+    def __init__self(self,value):
+        newnode=Node(value)
+        self.head=newnode
+        self.tail=newnode
+        self.length=1

4770a20c0f8027b5a16ef3e9217cf2c27e284697
shrujankulkarni <shrujan19@gmail.com>
2026-01-20 22:38:21 +0530

    .

--- REVISION/ll.py
+++ REVISION/ll.py
@@ -17,6 +17,24 @@ class LinkedList:
             print(temp.value)
             temp=temp.next
 
+    def append(self,value):
+        newnode= Node(value)
+        if self.length==0:
+            self.head=newnode
+            self.tail=newnode
+            self.length+=1
+        else:
+            self.tail.next=newnode
+            self.tail=newnode
+        return True
 
 ll=LinkedList(10)
 ll.printlist()
+print("----- After Append -----")
+ll.append(11)
+ll.append(12)
+ll.append(13)
+ll.append(14)
+ll.printlist()
+
+

e10ebd0ac7c603461f3a003febad9b9241c01a59
shrujankulkarni <shrujan19@gmail.com>
2026-01-24 22:18:02 +0530

    .

--- 02.Stacks/stack_constructor.py
+++ 02.Stacks/stack_constructor.py
@@ -2,4 +2,20 @@ class Node:
     def __init__(self,value):
         self.value=value
         self.next=None
-        
+
+
+class Stack:
+    def __init__(self,value):
+        newnode=Node(value)
+        self.top=newnode
+        self.height=1
+
+    def printstack(self):
+        temp=self.top
+        while(temp):
+            print(temp.value)
+            temp=temp.next
+
+
+obj=Stack(9)
+obj.printstack()

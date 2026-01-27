249008afe03e8d0d4bc62c904f100cd33e0df5f6
shrujankulkarni <shrujan19@gmail.com>
2026-01-27 14:38:46 +0530

    .

--- REVISION/ll_revision.py
+++ REVISION/ll_revision.py
@@ -14,13 +14,45 @@ class Linkedlist:
         temp=self.head
         while temp:
             print(temp.value)
-            print("Length:",self.length)
+            # print("Length:",self.length)
             temp=temp.next
 
-    # def append(self,value):
+    def append(self,value):
+        newnode=Node(value)
+        if self.length==0:
+                self.head=newnode
+                self.tail=newnode
+                self.length=1
+        else:
+             self.tail.next=newnode
+             self.tail=newnode
+             self.length+=1
+    
+    def pop(self):
+        if self.length==0:
+             return None
+        else:
+            temp=self.head
+            pre=self.head
+            while temp:
+                  pre=temp
+                  temp=temp.next
+            pre.next =None
+            return temp.value
+        
+            
+
+              
 
 
 ob= Linkedlist(10)
+ob.printlist()
+print("After append")
+ob.append(11)
+ob.append(12)
+ob.printlist()
+
+print("Popped value ",ob.pop())
 ob.printlist()
 
 

44df64f335a0c1bb30e9e027fbca2bd174b22a0a
shrujankulkarni <shrujan19@gmail.com>
2026-02-16 21:42:54 +0530

    Added Linkedlist constructor

--- ll_revisit.py
+++ ll_revisit.py
@@ -3,3 +3,9 @@ class Node:
         self.value= value
         self.next=None
 
+class Linkedlist:
+    def __init__(self,value):
+        newnode=Node(value)
+        self.head=newnode
+        self.tail=newnode
+        self.height=1

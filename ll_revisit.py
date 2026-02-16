3c517f079db3adff2241ad211f8a00f6cf000346
shrujankulkarni <shrujan19@gmail.com>
2026-02-16 21:47:37 +0530

    added append

--- ll_revisit.py
+++ ll_revisit.py
@@ -17,5 +17,18 @@ class Linkedlist:
             temp=temp.next
         print(f"Length= {self.height}")
 
+
+    def append(self,value):
+        if self.height==0:
+            newnode=Node(value)
+            self.head=newnode
+            self.tail=newnode
+            self.height=1            
+        else:
+            newnode=Node(value)
+            self.tail.next=newnode
+            self.tail=newnode
+            
+
 myobj=Linkedlist(10)
 myobj.printlist()

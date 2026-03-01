e4f5b433e436122e0c8381638c53e72b66290c26
Shrujan Kulkarni <shrujan19@gmail.com>
2026-03-01 19:31:04 +0530

    Update dll_constructor.py

--- dll_constructor.py
+++ dll_constructor.py
@@ -4,6 +4,7 @@ class Node:
         self.next=None
         self.prev=None
 
+
 class DoublyLinkedList:
     def __init__(self,value):
         newnode=Node(value)
@@ -11,11 +12,13 @@ class DoublyLinkedList:
         self.tail=newnode
         self.length=1
 
+
     def printlist(self):
         curr=self.head
         while curr is not None:
             print(curr.value)
             curr=curr.next
+
 
 dll=DoublyLinkedList(9)
 dll.printlist()

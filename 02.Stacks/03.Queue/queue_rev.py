f312b83237c39c7fd4549e3c8bbc1c79137a59f1
shrujankulkarni <shrujan19@gmail.com>
2026-03-02 21:22:09 +0530

    After Enqueue

--- 02.Stacks/03.Queue/queue_rev.py
+++ 02.Stacks/03.Queue/queue_rev.py
@@ -18,7 +18,28 @@ class Queue:
         while temp:
             print(temp.value)
             temp=temp.next
+    
+
+    def enqueue(self,value):
+        if self.length==0:
+            newnode=Node(value)
+            self.first=newnode
+            self.last=newnode
+        else:
+            temp=self.last
+            self.last.next=newnode
+            self.last=newnode
+        self.length+=1
+
+
+
 
 
 myobj=Queue(101)
+myobj.printlist()
+
+print("--- After Enqueue --- ")
+myobj.enqueue(11)
+myobj.enqueue(12)
+myobj.enqueue(13)
 myobj.printlist()

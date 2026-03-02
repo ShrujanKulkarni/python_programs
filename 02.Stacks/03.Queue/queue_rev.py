03320bc6244f888452a7c849bf45d43836de139a
shrujankulkarni <shrujan19@gmail.com>
2026-03-02 20:46:39 +0530

    Added Queue class

--- 02.Stacks/03.Queue/queue_rev.py
+++ 02.Stacks/03.Queue/queue_rev.py
@@ -2,3 +2,14 @@ class Node:
     def __init__(self,value):
         self.value=value
         self.next=None
+
+
+
+class Queue:
+    def __init__(self,value):
+        newnode= Node(value)
+        self.first=newnode
+        self.last=newnode
+        self.length=1
+
+        

3114ed161e8a674f0d1e96298e8aebba61bf27a8
shrujankulkarni <shrujan19@gmail.com>
2026-03-02 21:28:34 +0530

    .

--- 02.Stacks/03.Queue/queue_rev.py
+++ 02.Stacks/03.Queue/queue_rev.py
@@ -40,7 +40,7 @@ class Queue:
         elif self.length==1:
             self.first=None
             self.last=None
-            
+
         else:
             self.first=self.first.next
             temp.next=None
@@ -60,4 +60,7 @@ myobj.enqueue(13)
 myobj.printlist()
 
 print("--- After Dequeue --- ")
+myobj.dequeue()
+# myobj.dequeue()
+myobj.printlist()
 

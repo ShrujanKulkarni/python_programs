e529768865f48031bce0034d538b7c6221081a9b
shrujankulkarni <shrujan19@gmail.com>
2026-03-02 21:27:40 +0530

    Added dequeue

--- 02.Stacks/03.Queue/queue_rev.py
+++ 02.Stacks/03.Queue/queue_rev.py
@@ -33,6 +33,20 @@ class Queue:
         self.length+=1
 
 
+    def dequeue(self):
+        temp=self.first
+        if self.length==0:
+            return False
+        elif self.length==1:
+            self.first=None
+            self.last=None
+            
+        else:
+            self.first=self.first.next
+            temp.next=None
+            self.length-=1
+        
+
 
 
 
@@ -44,3 +58,6 @@ myobj.enqueue(11)
 myobj.enqueue(12)
 myobj.enqueue(13)
 myobj.printlist()
+
+print("--- After Dequeue --- ")
+

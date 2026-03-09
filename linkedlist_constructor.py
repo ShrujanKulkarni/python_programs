9831b93a41b286436e706f9f286c904ce5e70329
Shrujan Kulkarni <shrujan19@gmail.com>
2026-03-09 21:54:13 +0530

    Update linkedlist_constructor.py

--- linkedlist_constructor.py
+++ linkedlist_constructor.py
@@ -10,8 +10,10 @@ class LinkedList:
         self.tail=newnode
         self.length=1
 
+
 mylinkedlist = LinkedList(10)
 print(mylinkedlist.head.value)
+
 
 def printlist(self):
     temp=self.head
